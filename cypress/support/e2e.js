b4010065306b8f465b3563230938e19dd8c2ea1e
AQR88 <timahaua@gmail.com>
2025-06-08 21:24:14 +0300

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,4 +14,15 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
+// cypress/support/e2e.js
+
+Cypress.on("uncaught:exception", (err) => {
+  // Якщо текст помилки містить "Cloud provider detected"
+  if (err.message.includes("Cloud provider detected")) {
+    // Ігнорувати цю помилку, щоб тест не провалювався
+    return false;
+  }
+  // Інші помилки пропускати, щоб Cypress повідомляв про них
+  return true;
+});
