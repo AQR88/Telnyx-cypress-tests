8f4b3b5f9f7e1e27228513c34e20c8deaa362a65
AQR88 <timahaua@gmail.com>
2025-06-08 21:04:38 +0300

    pipeline
    created pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'i7p4m2',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
